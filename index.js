2a84172b3966f01a770ecc1fc75194e945318a04
Holden Turman <holden.turman1@gmail.com>
2024-05-10 16:45:50 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let content = document.getElementsByTagName("body")[0];
+let darkMode = document.getElementById("dark-change");
+
+darkMode.addEventListener("click", function () {
+  darkMode.classList.toggle("active");
+  content.classList.toggle("night");
+});
